1ebf21dde71cc40ae3769056620a621e249459d2
mrloop <ewan@mrloop.com>
2021-01-21 09:23:11 +0000

    feat(eq): add helper `eq`

---
+++ tests/integration/helpers/eq-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | eq', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it equals', async function(assert) {
+    this.set('bankName', 'qonto');
+
+    await render(hbs`{{#if (eq this.bankName 'qonto')}}Good{{else}}Bad{{/if}}`);
+
+    assert.equal(this.element.textContent.trim(), 'Good');
+
+    this.set('bankName', 'other bank');
+
+    assert.equal(this.element.textContent.trim(), 'Bad');
+  });
+});
