0a5dbe46e8720a9e376aee2c0dcb8685dbc4a54e
Michele Gerarduzzi <michele.gerarduzzi@qonto.com>
2023-11-08 18:50:32 +0100

    fix: remove curly component invocation

--- test-app/tests/helpers/clean-html.js
+++ test-app/tests/helpers/clean-html.js
@@ -1,8 +1,14 @@
 export default function cleanHtml({ element }) {
   let elementHelperIds = / id="[a-zA-Z0-9]*"/g;
   let emberViewClass = / class="ember-view"/g;
+  let extraWhiteSpace = /\s{2,}/g;
+  let spaceBetweenTags = />(\s+)</g;
+  let spaceAroundContent = />(\s+)([^<]+)(\s+)</g;
 
   return element.innerHTML
     .replace(elementHelperIds, '')
-    .replace(emberViewClass, '');
+    .replace(emberViewClass, '')
+    .replace(extraWhiteSpace, ' ')
+    .replace(spaceBetweenTags, '><')
+    .replace(spaceAroundContent, '>$2<');
 }
