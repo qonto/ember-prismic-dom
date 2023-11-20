0f2f23de2022712198ef7bcfc2d1057c2f60635b
Michele Gerarduzzi <michele.gerarduzzi@qonto.com>
2023-11-20 15:16:55 +0100

    feat: add template registry for addon

--- ember-prismic-dom/src/unpublished-development-types/index.d.ts
+++ ember-prismic-dom/src/unpublished-development-types/index.d.ts
@@ -2,8 +2,15 @@ import '@glint/environment-ember-loose';
 import 'ember-source/types';
 import 'ember-source/types/preview';
 
+import type EmberElementHelperRegistry from 'ember-element-helper/template-registry';
+import type { EmbroiderUtilRegistry } from '@embroider/util';
+import type TemplatesRegistry from '../template-registry';
+
 declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
+  export default interface Registry
+    extends EmbroiderUtilRegistry,
+      EmberElementHelperRegistry,
+      TemplatesRegistry {
     // Add any registry entries from other addons here that your addon itself uses (in non-strict mode templates)
     // See https://typed-ember.gitbook.io/glint/using-glint/ember/using-addons
   }
