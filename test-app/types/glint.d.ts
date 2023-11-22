708aa4c601c9d867e1f06de0ba53b6ee3f8727bb
Michele Gerarduzzi <michele.gerarduzzi@qonto.com>
2023-11-22 12:44:18 +0100

    test: extend app registry from addon’s

--- test-app/types/glint.d.ts
+++ test-app/types/glint.d.ts
@@ -1,5 +1,6 @@
 import '@glint/environment-ember-loose';
+import type PrismicDomRegistry from 'ember-prismic-dom/template-registry';
 
 declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {}
+  export default interface Registry extends PrismicDomRegistry {}
 }
